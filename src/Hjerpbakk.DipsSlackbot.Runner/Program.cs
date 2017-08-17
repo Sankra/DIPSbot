0a16fbf4b98e6f926d2850f144bb3df6936b5610
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-08-17 15:24:01 +0200

    Added runner project

---
+++ src/Hjerpbakk.DipsSlackbot.Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hjerpbakk.DipsSlackbot.Runner
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
