647eb2cec573a73d288a01a85f6da0c8f437e369
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-09-27 13:52:50 +0200

    Azure script

--- azure.sh
+++ azure.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# Needs: brew install azure-cli
+# Push to Azure Container Registry
+az group create --name kitchen-responsible-rg --location westeurope
+az acr create --name dipsbot --resource-group kitchen-responsible-rg --admin-enabled --sku Basic
+az acr login --name dipsbot
+az acr list --resource-group kitchen-responsible-rg --query "[].{acrLoginServer:loginServer}" --output table
 # -> dipsbot.azurecr.io
-docker tag dipsbot dipsbot.azurecr.io/dipsbot
-docker push dipsbot.azurecr.io/dipsbot
+docker tag kitchen-responsible dipsbot.azurecr.io/kitchen-responsible
 
-az container delete --name dipsbot --resource-group kitchen-responsible-rg
+# Run container locally
+# docker run -p 5000:80 dipsbot.azurecr.io/kitchen-responsible
+docker push dipsbot.azurecr.io/kitchen-responsible
 
+# Run in Azure Container Instances
+az acr show --name dipsbot --query loginServer
 az acr credential show --name dipsbot --query "passwords[0].value"
-az container create --name dipsbot --image dipsbot.azurecr.io/dipsbot --cpu 1 --memory 1 --registry-password [PASSWORD] --ip-address public -g kitchen-responsible-rg
+az container create --name kitchen-responsible-service --image dipsbot.azurecr.io/kitchen-responsible --cpu 1 --memory 1 --registry-password [PASSWORD] --ip-address public -g kitchen-responsible-rg
+az container show --name kitchen-responsible-service --resource-group kitchen-responsible-rg --query state
+az container show --name kitchen-responsible-service --resource-group kitchen-responsible-rg --query ipAddress.ip
