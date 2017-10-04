72af99f1d77f5e664998bd9fd276fe62ec127208
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-10-04 11:16:19 +0200

    Added TODO

--- src/Hjerpbakk.DIPSBot/Actions/IActions.cs
+++ src/Hjerpbakk.DIPSBot/Actions/IActions.cs
@@ -5,6 +5,7 @@ namespace Hjerpbakk.DIPSBot.Actions
 {
     interface IAction
     {
+        // TODO: Legg inn  muligheten for å både se (for vanlige brukere) og oppdatere hvilke tegneserier som skal vises..
         Task Execute(SlackMessage message);
     }
 }
