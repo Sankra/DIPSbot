bf5650815ea7f6f9b3e9e9acb2ccae62ebbc525b
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-10-23 20:16:42 +0200

    Simplified build script

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 rm -r ./publish
 set -e
-dotnet restore
-dotnet build
 dotnet publish src/Hjerpbakk.DIPSBot.Runner/Hjerpbakk.DIPSBot.Runner.csproj -o ../../publish -c Release
 docker build -t dipsbot .
 
